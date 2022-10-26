409c5aa16b12fc8a50a7802f0357ef5547baa346
Miriam <miriamjoshua00@gmail.com>
2022-10-25 23:32:19 -0400

    commit tree 1b45908774c921cdd9e1ae3ce3d6ff0b0a1d8217

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+    #Custom Validation
+    validate :title_is_clickbait
+
+    KEYWORDS = [
+        /Won't Believe/i, 
+        /Secret/i, 
+        /Top \d/i, 
+        /Guess/i
+    ]
+
+    def title_is_clickbait
+        
+        if KEYWORDS.none? { |word| word.match title}
+            errors.add(:title, "must be a clickbaity title")
+        end
+    end
 end
